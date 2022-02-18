e749afc8637bfd235d4da4bef94cf70cf2cfb839
anandani4136 <anandani4136@students.d211.org>
2022-02-17 23:32:09 -0600

    Minor Updates

--- src/components/FAQCard.js
+++ src/components/FAQCard.js
@@ -5,7 +5,7 @@ const FAQCard = ({title, items}) => {
     return (
         <div className="rounded-xl bg-gradient-to-l from-red-700 to-blue-700 p-1 h-94">
             <div className="justify-center h-full bg-white text-white rounded-lg">
-                <p className='grid place-items-center text-black text-2xl overflow-x:scroll'>{title}</p>
+                <p className='grid place-items-center text-blue-900 text-2xl font-bold overflow-x:scroll'>{title}</p>
                 {items.map((animal, index) => (
                     <div>
                         <p className='text-black text-bold text-xl ml-2' key={index}>{animal.question}</p>
