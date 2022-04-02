efb07b075487493489cee631b8043ade6f529421
anandani4136 <anandani4136@students.d211.org>
2022-04-02 14:54:44 -0500

    Footer Update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='flex flex-row items-center bg-gradient-to-tl from-red-700 to-blue-700 px-4 lg:px-32 lg:py-8 py-4 mt-10'>
-            <p className='text-md text-white font-semibold'>Website created by Saksham Gupta from</p>
+            <p className='text-md text-white font-semibold'>Website created by Saksham Gupta and Ronit Anandani from</p>
             <a href="https://conant.hackclub.com"><img src='chc.png' width="200" height="35" alt='chc'></img></a>
         </div>
     );
